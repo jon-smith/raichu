f6a0cea190a62ddcb0f54e87678fab4d8a30bdb9
jon-smith <jonsmith24601@outlook.com>
2019-11-26 19:19:09 +0000

    generic datapoints for xy plot data viewer passes in

--- src/renderer/components/test-data-viewer.tsx
+++ src/renderer/components/test-data-viewer.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import XYPlot from '@ui/charts/xy-plot';
 
+const data = [1,2,3,4].map(x => ({x, y: x}));
+
 const TestDataViewer = () => {
 	return (
 		<div className="test-data-viewer">
-			<XYPlot className="test-data-chart" />
+			<XYPlot 
+				className="test-data-chart" 
+				series={[{name: "", data: data}]}
+			/>
 		</div>
 	)
 }
--- src/ui/charts/xy-plot.tsx
+++ src/ui/charts/xy-plot.tsx
@@ -1,14 +1,31 @@
 import * as React from 'react'
+import {useMemo} from 'react'
 import {FlexibleXYPlot, HorizontalGridLines, VerticalGridLines, XAxis, YAxis, LineMarkSeries} from 'react-vis'
 
-interface Props
-{
-	className?: string;
+export type DataPoint = {
+	x: number; 
+	y: number
+};
+
+export type DataSeriesT<DataPointT extends DataPoint> = {
+	data: DataPointT[];
 }
 
-const XYPlot = (props: Props) =>
+interface Props<DataPointT extends DataPoint>
 {
-	const data = [1,2,3,4].map(x => ({x, y: x}));
+	className?: string;
+	series: DataSeriesT<DataPointT>[];
+}
+
+const buildSeriesComponents = 
+<DataPointT extends DataPoint>(series: DataSeriesT<DataPointT>[]) => {
+	return series.map((s, i) => <LineMarkSeries key={i} data={s.data}/>)
+}
+
+const XYPlot = <DataPointT extends DataPoint>(props: Props<DataPointT>) =>
+{
+	const {series} = props;
+	const seriesComponents = useMemo(() => buildSeriesComponents(series), [series]);
 
 	return (
 		<FlexibleXYPlot className={props.className}>
@@ -16,7 +33,7 @@ const XYPlot = (props: Props) =>
 			<VerticalGridLines />
 			<XAxis title={"X"}/>
 			<YAxis title={"Y"}/>
-			<LineMarkSeries data={data}/>
+			{seriesComponents}
 		</FlexibleXYPlot>
 	)
 }
