51be1b4eb03ad025f09644a431573a9e6820ae85
jon-smith <jonsmith24601@outlook.com>
2019-11-28 19:13:17 +0000

    display HR on plot when loading GPX files

--- src/renderer/components/test-data-viewer.tsx
+++ src/renderer/components/test-data-viewer.tsx
@@ -1,9 +1,8 @@
-import * as React from 'react'
-import { useState } from 'react'
+import * as React from 'react';
+import { useState } from 'react';
+import * as lodash from 'lodash';
 import XYPlot from '@ui/charts/xy-plot';
 import GpxFileDrop, { FileAndGpx } from './gpx-file-drop';
-
-const data = [1, 2, 3, 4].map(x => ({ x, y: x }));
 
 const TestDataViewer = () => {
 
@@ -17,11 +16,25 @@ const TestDataViewer = () => {
 		</div>
 	));
 
+	const series = loadedFiles.map(l => {
+
+		const heartRateAndTime = lodash.flatten(lodash.flatten(
+			l.gpx.track.segments.map(s => s.points.map(p => ({hr: p.heartRate, t: p.time})))));
+
+		const earliestTime = lodash.min(heartRateAndTime.map(hrt => hrt.t)) ?? new Date();
+
+		const dataPoints = heartRateAndTime
+			.filter(hrt => hrt.hr != undefined)
+			.map(hrt => ({x: (hrt.t.getTime() - earliestTime.getTime()), y: hrt.hr ?? 0}));
+
+		return {name: "", data: dataPoints}
+	});
+
 	return (
 		<div className="test-data-viewer">
 			<XYPlot
 				className="test-data-chart"
-				series={[{ name: "", data: data, color: 'red' }]}
+				series={series}
 			/>
 			<GpxFileDrop
 				loadedFiles={loadedFiles}
