1344488cf6d36a1815c17e5e3838480511fcf119
jon-smith <jonsmith24601@outlook.com>
2019-11-28 17:34:59 +0000

    add zoom to xy plot

--- src/ui/charts/xy-plot.tsx
+++ src/ui/charts/xy-plot.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
-import {useMemo} from 'react'
-import {FlexibleXYPlot, HorizontalGridLines, VerticalGridLines, XAxis, YAxis, LineMarkSeries} from 'react-vis'
+import { useMemo, useState, useCallback } from 'react'
+import { FlexibleXYPlot, HorizontalGridLines, VerticalGridLines, XAxis, YAxis, LineMarkSeries, Highlight } from 'react-vis'
+
+type ReactVisArea =
+	{
+		left: number;
+		right: number;
+		top: number;
+		bottom: number;
+	}
 
 export type DataPoint = {
-	x: number; 
+	x: number;
 	y: number;
 };
 
@@ -12,32 +20,51 @@ export type DataSeriesT<DataPointT extends DataPoint> = {
 	data: DataPointT[];
 }
 
-interface Props<DataPointT extends DataPoint>
-{
+interface Props<DataPointT extends DataPoint> {
 	className?: string;
 	series: DataSeriesT<DataPointT>[];
 }
 
-const buildSeriesComponents = 
-<DataPointT extends DataPoint>(series: DataSeriesT<DataPointT>[]) => {
-	return series.map((s, i) => <LineMarkSeries key={i} data={s.data}/>)
-}
+const buildSeriesComponents =
+	<DataPointT extends DataPoint>(series: DataSeriesT<DataPointT>[]) => {
+		return series.map((s, i) => <LineMarkSeries key={i} data={s.data} />)
+	}
 
-const gridStyle = {stroke: 'lightgrey'};
-const axisStyle = {line: {stroke: 'black'}};
+const gridStyle = { stroke: 'lightgrey' };
+const axisStyle = { line: { stroke: 'black' } };
 
-const XYPlot = <DataPointT extends DataPoint>(props: Props<DataPointT>) =>
-{
-	const {series} = props;
+const XYPlot = <DataPointT extends DataPoint>(props: Props<DataPointT>) => {
+	const { series } = props;
+
+	const [zoomArea, setZoomArea] = useState<ReactVisArea | null>(null);
+	const updateZoomArea = useCallback((area: ReactVisArea) => {
+		setZoomArea({
+			bottom: zoomArea?.bottom ?? 0 + (area.top - area.bottom),
+			left: zoomArea?.left ?? 0 - (area.right - area.left),
+			right: zoomArea?.right ?? 0 - (area.right - area.left),
+			top: zoomArea?.top ?? 0 + (area.top - area.bottom)
+		});
+	}, [zoomArea]);
+
+	const xDomain = zoomArea && [zoomArea.left, zoomArea.right];
+	const yDomain = zoomArea && [zoomArea.bottom, zoomArea.top];
 	const seriesComponents = useMemo(() => buildSeriesComponents(series), [series]);
 
 	return (
-		<FlexibleXYPlot className={props.className}>
-			<HorizontalGridLines style={gridStyle}/>
-			<VerticalGridLines style={gridStyle}/>
-			<XAxis title={"X"} style={axisStyle}/>
-			<YAxis title={"Y"} style={axisStyle}/>
+		<FlexibleXYPlot
+			className={props.className}
+			xDomain={xDomain}
+			yDomain={yDomain}
+		>
+			<HorizontalGridLines style={gridStyle} />
+			<VerticalGridLines style={gridStyle} />
 			{seriesComponents}
+			<XAxis title={"X"} style={axisStyle} />
+			<YAxis title={"Y"} style={axisStyle} />
+			<Highlight
+				onBrushEnd={(area: ReactVisArea) => setZoomArea(area)}
+				onDrag={(area: ReactVisArea) => updateZoomArea(area)}
+			/>
 		</FlexibleXYPlot>
 	)
 }
