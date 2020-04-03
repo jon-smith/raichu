145ddb2abff27c3ff1b1bbde2259e3bdaee44e22
jon-smith <jonsmith24601@outlook.com>
2020-04-03 12:42:45 +0100

    variable widths and fixed continue x axis

--- src/renderer/components/workout-creator-chart.tsx
+++ src/renderer/components/workout-creator-chart.tsx
@@ -2,31 +2,44 @@ import * as React from 'react';
 import { useRef, useEffect, useState } from 'react';
 import * as d3 from 'd3';
 
-type DataItem = { category: string; value: number; color: string };
+type Interval = {
+	category: string;
+	intensity: number;
+	length: number;
+	color: string;
+};
+
+type IntervalChartItem = Interval & { startTime: number };
+
+const calculateStartTimes = (intervals: Interval[]): IntervalChartItem[] => {
+	const copy = intervals.map(i => ({ ...i, startTime: 0 }));
+	for (let i = 0; i < intervals.length; ++i) {
+		copy[i].startTime = i === 0 ? 0 : copy[i - 1].startTime + copy[i - 1].length;
+	}
+	return copy;
+};
 
 const buildChart = (
 	nodeRef: SVGSVGElement,
 	width: number,
 	height: number,
-	initialData: DataItem[],
-	updateData: (d: DataItem[]) => void
+	initialData: Interval[],
+	updateData: (d: Interval[]) => void
 ) => {
 	const svg = d3.select(nodeRef).html('');
 
-	const padding = { top: 20, left: 20, right: 20, bottom: 20 };
+	const padding = { top: 20, left: 40, right: 20, bottom: 20 };
 
-	const xScale = d3.scaleBand();
+	const xScale = d3.scaleLinear();
 	const yScale = d3.scaleLinear();
 
-	const xAxis = d3.axisBottom(xScale);
-
-	const data = initialData.map(d => ({ ...d }));
+	const data = calculateStartTimes(initialData);
 
 	let startX: number;
 	let xDiff: number;
 
 	const drag = d3
-		.drag<SVGRectElement, DataItem>()
+		.drag<SVGRectElement, IntervalChartItem>()
 		.on('start', (d, i, j) => {
 			const bar = d3.select(j[i]);
 			const mouseX = d3.mouse(j[i])[0];
@@ -48,8 +61,8 @@ const buildChart = (
 			const mouseX = d3.mouse(j[i])[0];
 			const newX = mouseX - xDiff;
 
-			const xRange = (xScale.step() * 2) / 3;
-			const backlash = xScale.bandwidth() / 2.5;
+			const xRange = xScale(d.length) / 2; // (xScale.step() * 2) / 3;
+			const backlash = xScale(d.length) / 2;
 
 			if (newX < xScale.range()[0] - backlash || xScale.range()[1] - backlash < newX) return;
 
@@ -65,8 +78,8 @@ const buildChart = (
 			})();
 
 			const nearestBar = d3
-				.selectAll<SVGRectElement, DataItem>('.bar')
-				.filter((d2: DataItem) => d2 === data[nearestIdx]);
+				.selectAll<SVGRectElement, IntervalChartItem>('.bar')
+				.filter((d2: Interval) => d2 === data[nearestIdx]);
 
 			const nearestX = parseFloat(nearestBar.attr('x'));
 
@@ -78,47 +91,48 @@ const buildChart = (
 				data[curIdx] = data[nearestIdx];
 				data[nearestIdx] = tmp;
 
+				// Update start times
+				for (let k = 0; k < data.length; ++k) {
+					data[k].startTime = k === 0 ? 0 : data[k - 1].startTime + data[k - 1].length;
+				}
+
 				startX = nearestX;
-
-				xScale.domain(data.map(d2 => d2.category));
-
-				svg
-					.select<SVGSVGElement>('.x.axis')
-					.transition()
-					.duration(200)
-					.call(d3.axisBottom(xScale));
 
 				nearestBar
 					.transition()
 					.duration(100)
-					.attr('x', xScale(nearestBar.datum().category) ?? 0);
+					.attr('x', xScale(nearestBar.datum().startTime) ?? 0);
 			}
 		})
-		.on('end', (d: DataItem, i, j) => {
+		.on('end', (d, i, j) => {
 			const bar = d3.select(j[i]);
 			bar
 				.transition()
 				.duration(300)
-				.attr('x', xScale(d.category) ?? 0)
+				.attr('x', xScale(d.startTime) ?? 0)
 				.on('end', () => updateData(data));
 		});
 
-	xScale
-		.domain(data.map(d => d.category))
-		.rangeRound([padding.left, width - padding.right])
-		.padding(0);
+	const xAxis = d3.axisBottom(xScale);
+	const yAxis = d3.axisLeft(yScale).tickFormat(d3.format('.0%'));
 
-	yScale.domain([0, d3.max(data, d => d.value) ?? 0]).range([height - padding.bottom, padding.top]);
+	xScale
+		.domain([0, d3.max(data, d => d.startTime + d.length) ?? 0])
+		.range([padding.left, width - padding.right]);
+
+	yScale
+		.domain([0, d3.max(data, d => d.intensity) ?? 0])
+		.range([height - padding.bottom, padding.top]);
 
 	svg
 		.selectAll('rect')
 		.data(data)
 		.enter()
 		.append('rect')
-		.attr('width', xScale.bandwidth())
-		.attr('height', d => height - padding.bottom - yScale(d.value))
-		.attr('x', d => xScale(d.category) ?? 0)
-		.attr('y', d => yScale(d.value) ?? 0)
+		.attr('width', d => xScale(d.length) - padding.left)
+		.attr('height', d => height - padding.bottom - yScale(d.intensity))
+		.attr('x', d => xScale(d.startTime))
+		.attr('y', d => yScale(d.intensity))
 		.attr('fill', d => d.color)
 		.attr('class', 'bar')
 		.call(drag);
@@ -128,16 +142,22 @@ const buildChart = (
 		.attr('transform', `translate(${0},${height - padding.bottom})`)
 		.attr('class', 'x axis')
 		.call(xAxis);
+
+	svg
+		.append('g')
+		.attr('transform', `translate(${padding.left},${0})`)
+		.attr('class', 'y axis')
+		.call(yAxis);
 };
 
 const WorkoutCreatorChart = () => {
 	const svgRef = useRef<SVGSVGElement>(null);
 
-	const [data, setData] = useState<DataItem[]>([
-		{ category: 'A', value: 50, color: d3.schemeCategory10[0] },
-		{ category: 'B', value: 30, color: d3.schemeCategory10[1] },
-		{ category: 'C', value: 20, color: d3.schemeCategory10[2] },
-		{ category: 'D', value: 20, color: d3.schemeCategory10[3] }
+	const [data, setData] = useState<Interval[]>([
+		{ category: 'A', intensity: 0.5, length: 60, color: d3.schemeBlues[3][0] },
+		{ category: 'B', intensity: 0.3, length: 60, color: d3.schemeBlues[3][1] },
+		{ category: 'C', intensity: 0.2, length: 60, color: d3.schemeBlues[3][2] },
+		{ category: 'D', intensity: 0.2, length: 60, color: d3.schemeBlues[3][3] }
 	]);
 
 	const width = 400;
