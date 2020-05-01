55b7f0e4620016fe7a997c710b878b5bae0f90bb
jon-smith <jonsmith24601@outlook.com>
2020-05-01 17:41:35 +0100

    show power on activity loader

--- src/renderer/components/workout-creator/activity-loader.tsx
+++ src/renderer/components/workout-creator/activity-loader.tsx
@@ -1,22 +1,29 @@
 import * as React from 'react';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 
 import Box from '@material-ui/core/Box';
 import IconButton from '@material-ui/core/IconButton';
 import { Close } from '@material-ui/icons';
 
-import XYPlot from 'ui/charts/xy-plot';
+import XYPlot, { DataSeriesT } from 'ui/charts/xy-plot';
 import GpxFileDrop, { FileAndGpx } from 'ui/file/gpx-file-drop';
 
 import { formatSecondsAsHHMMSS } from 'shared/utils/time-format-utils';
 import { buildNiceTimeTicksToDisplay } from 'shared/utils/chart-utils';
 
 import { useDispatchCallback } from 'state/dispatch-hooks';
-import { setActivity, clearActivity } from 'state/workout-creator/workout-creator-slice';
+import {
+	setActivity,
+	clearActivity,
+	getActivityPowerPerSecond
+} from 'state/workout-creator/workout-creator-slice';
 import { useWorkoutCreatorSelector } from 'state/reducers';
 
 const ActivityLoader = () => {
-	const loadedActivity = useWorkoutCreatorSelector(s => s.activity);
+	const { loadedActivity, powerData } = useWorkoutCreatorSelector(s => ({
+		loadedActivity: s.activity,
+		powerData: getActivityPowerPerSecond(s)
+	}));
 
 	const setActivityDispatcher = useDispatchCallback(setActivity);
 	const clearActivitityDispatcher = useDispatchCallback(clearActivity);
@@ -29,6 +36,17 @@ const ActivityLoader = () => {
 			}
 		},
 		[setActivityDispatcher]
+	);
+
+	const powerDataSeries = useMemo(
+		(): DataSeriesT[] => [
+			{
+				name: 'power-vs-time',
+				data: powerData.map((p, i) => ({ x: i, y: p })),
+				seriesType: 'line'
+			}
+		],
+		[powerData]
 	);
 
 	return (
@@ -48,9 +66,9 @@ const ActivityLoader = () => {
 			<Box>
 				<XYPlot
 					className="test-data-chart"
-					series={[]}
+					series={powerDataSeries}
 					xTickFormat={formatSecondsAsHHMMSS}
-					xTickValues={buildNiceTimeTicksToDisplay(3600, 5)}
+					xTickValues={buildNiceTimeTicksToDisplay(powerData.length, 5)}
 					xAxisLabel="time"
 					yAxisLabel="Power"
 				/>
