1697b5cbbae6ad003fe53fa81d0e62eecbf8c23d
jon-smith <jonsmith24601@outlook.com>
2020-05-04 11:26:21 +0100

    expansion on loaded activity

--- src/renderer/components/workout-creator/activity-loader.tsx
+++ src/renderer/components/workout-creator/activity-loader.tsx
@@ -4,6 +4,10 @@ import { useCallback, useMemo } from 'react';
 import Box from '@material-ui/core/Box';
 import IconButton from '@material-ui/core/IconButton';
 import { Close } from '@material-ui/icons';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 
 import XYPlot, { DataSeriesT } from 'ui/charts/xy-plot';
 import GpxFileDrop, { FileAndGpx } from 'ui/file/gpx-file-drop';
@@ -55,28 +59,40 @@ const ActivityLoader = () => {
 
 	return (
 		<>
-			<Box display="flex" flexDirection="row" alignItems="center" margin="10px" minHeight="2em">
-				{loadedActivity ? (
-					<>
-						<span>{`Loaded activity: ${loadedActivity.track.name}`}</span>
-						<IconButton aria-label="close file" onClick={() => clearActivitityDispatcher()}>
-							<Close />
-						</IconButton>
-					</>
-				) : (
-					<GpxFileDrop onAddFiles={addFiles} allowMultiple={false} />
-				)}
-			</Box>
-			<Box>
-				<XYPlot
-					className="test-data-chart"
-					series={powerDataSeries}
-					xTickFormat={formatSecondsAsHHMMSS}
-					xTickValues={buildNiceTimeTicksToDisplay(maxX, 5)}
-					xAxisLabel="time"
-					yAxisLabel="Power"
-				/>
-			</Box>
+			<ExpansionPanel>
+				<ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+					<Box
+						display="flex"
+						flexDirection="row"
+						alignItems="center"
+						minHeight="2em"
+						onClick={event => event.stopPropagation()}
+						onFocus={event => event.stopPropagation()}
+					>
+						{loadedActivity ? (
+							<>
+								<span>{`Loaded activity: ${loadedActivity.track.name}`}</span>
+								<IconButton aria-label="close file" onClick={() => clearActivitityDispatcher()}>
+									<Close />
+								</IconButton>
+							</>
+						) : (
+							<GpxFileDrop onAddFiles={addFiles} allowMultiple={false} />
+						)}
+					</Box>
+				</ExpansionPanelSummary>
+				<ExpansionPanelDetails>
+					<Box className="workout-creator-activity-data-chart">
+						<XYPlot
+							series={powerDataSeries}
+							xTickFormat={formatSecondsAsHHMMSS}
+							xTickValues={buildNiceTimeTicksToDisplay(maxX, 5)}
+							xAxisLabel="time"
+							yAxisLabel="Power"
+						/>
+					</Box>
+				</ExpansionPanelDetails>
+			</ExpansionPanel>
 		</>
 	);
 };
