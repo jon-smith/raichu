850cdcc74c4784eac4fec15808f454c015577d09
jon-smith <jonsmith24601@outlook.com>
2020-05-05 16:04:09 +0100

    ignore gpx paths in immutableCheck middleware

--- src/state/configureStore.ts
+++ src/state/configureStore.ts
@@ -4,10 +4,16 @@ import { rootReducer } from './reducers';
 
 const makeReducer = () => enableBatching(rootReducer);
 
+// Ignore gpx files because they are typically large datasets and slow the app down in dev mode
+const middlewareIgnorePaths = ['workoutCreator.activity', 'activities.files'];
+
 const configureStore = () => {
 	const store = toolkitConfigure({
 		reducer: makeReducer(),
-		middleware: getDefaultMiddleware({ serializableCheck: false }),
+		middleware: getDefaultMiddleware({
+			serializableCheck: false,
+			immutableCheck: { ignoredPaths: middlewareIgnorePaths }
+		}),
 		devTools: true
 	});
 
