c07e5987139ed6997d77f36f3a0c1e7e381b2db8
jon-smith <jonsmith24601@outlook.com>
2020-05-12 12:36:44 +0100

    turn off immutable check because it's slow

--- src/state/configure-store.ts
+++ src/state/configure-store.ts
@@ -4,15 +4,12 @@ import { rootReducer } from './reducers';
 
 const makeReducer = () => enableBatching(rootReducer);
 
-// Ignore gpx files because they are typically large datasets and slow the app down in dev mode
-const middlewareIgnorePaths = ['workoutCreator.activity', 'activities.files'];
-
 const configureStore = () => {
 	const store = toolkitConfigure({
 		reducer: makeReducer(),
 		middleware: getDefaultMiddleware({
 			serializableCheck: false,
-			immutableCheck: { ignoredPaths: middlewareIgnorePaths }
+			immutableCheck: false
 		}),
 		devTools: true
 	});
