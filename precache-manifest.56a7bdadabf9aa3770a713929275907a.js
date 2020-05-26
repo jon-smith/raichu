afe13863f9dd6a8bf09e8d8f8d746a950d353d88
jon-smith <jonsmith24601@outlook.com>
2020-05-26 07:34:53 +0100

    Updates

---
+++ precache-manifest.56a7bdadabf9aa3770a713929275907a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066aef8844839e3063418574ee8a60ab",
+    "url": "/raichu/index.html"
+  },
+  {
+    "revision": "124db4ab2c2f5ba56d99",
+    "url": "/raichu/static/css/2.6780e105.chunk.css"
+  },
+  {
+    "revision": "355cb1c51c9e8f79b1e1",
+    "url": "/raichu/static/css/main.6966074c.chunk.css"
+  },
+  {
+    "revision": "124db4ab2c2f5ba56d99",
+    "url": "/raichu/static/js/2.9d5eb776.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/raichu/static/js/2.9d5eb776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "355cb1c51c9e8f79b1e1",
+    "url": "/raichu/static/js/main.01d23f16.chunk.js"
+  },
+  {
+    "revision": "68f39151d7dac8cb20fd",
+    "url": "/raichu/static/js/runtime-main.c2ee86f9.js"
+  }
+]);
