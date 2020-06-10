4be68875e32105e53a031092a022fd2c11137c46
jon-smith <jonsmith24601@outlook.com>
2020-06-10 17:53:03 +0100

    best split plot into its own file

--- src/components/activity-view/best-split/best-split-view.tsx
+++ src/components/activity-view/best-split/best-split-view.tsx
@@ -1,38 +1,16 @@
 import React, { useMemo, useState } from 'react';
-import XYPlot, { DataSeriesT } from 'generic-components/charts/xy-plot';
 import * as activityCalculator from 'library/activity-data/activity-calculator';
 import { BestSplitOption } from 'library/activity-data/activity-calculator';
 import { ActivityContainer } from 'library/activity-data/activity-container';
-import { formatSecondsAsTimeWords } from 'library/utils/time-format-utils';
 import { useActivitySelector } from 'store/reducers';
 import { getSelectedActivity } from 'store/activity-data/selectors';
 import { primaryColourForBestSplitOption } from 'components/helpers/activity-data-component-helpers';
 import BestSplitCurveSelection from './best-split-curve-selection';
-
-function frontBack<T>(a: T[]) {
-	return [a[0], a[a.length - 1]] as const;
-}
-
-const defaultSecondTicks = [5, 30];
-
-const defaultMinuteTicks = [1, 2, 5, 10, 20, 30, 45];
-
-const defaultHourTicks = [1, 2, 5];
-
-const defaultTimeTicksForBestSplits = [
-	...defaultSecondTicks,
-	...defaultMinuteTicks.map((t) => t * 60),
-	...defaultHourTicks.map((t) => t * 60 * 60),
-];
-
-const defaultTimeAxisRange = frontBack(defaultTimeTicksForBestSplits);
-
-const distancesForPaceCurve = [100, 200, 400, 800, 1000, 1600, 5000, 10000];
-
-const defaultPaceCurveXDomain = frontBack(distancesForPaceCurve);
+import BestSplitPlot from './best-split-plot';
+import { distancesForBestSplits, timeIntervalsForBestSplits } from './best-split-x-values';
 
 function buildPaceCurve(d: ActivityContainer) {
-	const bestSplits = activityCalculator.getMinTimesPerDistance(d, distancesForPaceCurve);
+	const bestSplits = activityCalculator.getMinTimesPerDistance(d, distancesForBestSplits);
 
 	const bestSplitsDataPoints = bestSplits
 		.filter((r) => r.best !== null)
@@ -50,9 +28,12 @@ function buildPaceCurve(d: ActivityContainer) {
 }
 
 function buildPowerCurve(d: ActivityContainer) {
-	const timeIntervals = [1, 5, 10, 30, 60, 120, 240, 360, 600, 900];
-
-	const bestSplits = activityCalculator.getBestSplitsVsTime(d, 'power', timeIntervals, 10);
+	const bestSplits = activityCalculator.getBestSplitsVsTime(
+		d,
+		'power',
+		timeIntervalsForBestSplits,
+		10
+	);
 
 	const bestSplitsDataPoints = bestSplits.map((r) => ({
 		x: r.distance,
@@ -67,9 +48,12 @@ function buildPowerCurve(d: ActivityContainer) {
 }
 
 function buildHRCurve(d: ActivityContainer) {
-	const timeIntervals = [1, 5, 10, 30, 60, 120, 240, 360, 600, 900];
-
-	const bestSplits = activityCalculator.getBestSplitsVsTime(d, 'heartrate', timeIntervals, 10);
+	const bestSplits = activityCalculator.getBestSplitsVsTime(
+		d,
+		'heartrate',
+		timeIntervalsForBestSplits,
+		10
+	);
 
 	const bestSplitsDataPoints = bestSplits.map((r) => ({
 		x: r.distance,
@@ -81,90 +65,6 @@ function buildHRCurve(d: ActivityContainer) {
 		data: bestSplitsDataPoints,
 		color: primaryColourForBestSplitOption('heartrate'),
 	};
-}
-
-function calcYDomain(series: readonly DataSeriesT[]) {
-	let yRange = null as [number, number] | null;
-	series.forEach((s) =>
-		s.data.forEach((d) => {
-			if (d.y !== null) {
-				if (yRange == null) yRange = [d.y, d.y];
-				else {
-					if (d.y < yRange[0]) yRange[0] = d.y;
-					if (d.y > yRange[1]) yRange[1] = d.y;
-				}
-			}
-		})
-	);
-
-	if (yRange == null) return undefined;
-
-	const difference = yRange[1] - yRange[0];
-
-	const margin = difference === 0 ? yRange[0] * 0.1 : difference * 0.1;
-
-	return [yRange[0] - margin, yRange[1] + margin] as const;
-}
-
-type BestSplitPlotProps = Pick<
-	React.ComponentProps<typeof XYPlot>,
-	'xAxisLabel' | 'yAxisLabel' | 'xTickValues' | 'xTickFormat' | 'series'
-> & {
-	defaultXDomain: readonly [number, number];
-};
-
-const BestSplitPlot = (props: BestSplitPlotProps) => {
-	const { series, defaultXDomain, ...plotProps } = props;
-
-	const yDomain = useMemo(() => calcYDomain(series), [series]);
-
-	return (
-		<XYPlot
-			className="test-data-chart"
-			series={series}
-			xDomain={series.length === 0 ? defaultXDomain : undefined}
-			yDomain={yDomain}
-			xType="log"
-			{...plotProps}
-		/>
-	);
-};
-
-function bestSplitChartProps(o: BestSplitOption) {
-	switch (o) {
-		case 'heartrate':
-			return {
-				defaultXDomain: defaultTimeAxisRange,
-				xAxisLabel: 'time',
-				yAxisLabel: 'HR',
-				xTickFormat: formatSecondsAsTimeWords,
-				xTickValues: defaultTimeTicksForBestSplits,
-			};
-		case 'power':
-			return {
-				defaultXDomain: defaultTimeAxisRange,
-				xAxisLabel: 'time',
-				yAxisLabel: 'power',
-				xTickFormat: formatSecondsAsTimeWords,
-				xTickValues: defaultTimeTicksForBestSplits,
-			};
-		case 'speed':
-			return {
-				defaultXDomain: defaultPaceCurveXDomain,
-				xAxisLabel: 'distance',
-				yAxisLabel: 'pace',
-				xTickFormat: String,
-				xTickValues: distancesForPaceCurve,
-			};
-		default:
-			return {
-				defaultXDomain: defaultPaceCurveXDomain,
-				xAxisLabel: '',
-				yAxisLabel: '',
-				xTickFormat: String,
-				xTickValues: distancesForPaceCurve,
-			};
-	}
 }
 
 function useBestSplitChartData(o: BestSplitOption) {
@@ -192,7 +92,7 @@ export default function BestSplitPlotComponent() {
 	return (
 		<>
 			<BestSplitCurveSelection option={bestSplitOption} onChange={setBestSplitOption} />
-			<BestSplitPlot series={data} {...bestSplitChartProps(bestSplitOption)} />
+			<BestSplitPlot series={data} option={bestSplitOption} />
 		</>
 	);
 }
