c1788192ceb129c6b4bf9576fa17a13e80186374
jon-smith <26364802+jon-smith@users.noreply.github.com>
2025-05-17 08:37:28 +0100

    fix some warnings

--- src/library/utils/deferred.ts
+++ src/library/utils/deferred.ts
@@ -1,7 +1,7 @@
 // Creates a Promise<void> that will block until resolve is called
 export function makeDeferred() {
 	type Deferred = { promise: Promise<void>; resolve: () => void };
-	let deferred: Partial<Deferred> = {};
+	const deferred: Partial<Deferred> = {};
 	deferred.promise = new Promise((resolve) => {
 		deferred.resolve = resolve;
 	});
